9ac587b3bf134ce59c09bc28e4e18cc766acc0a0
nicolep1056 <nicolep1056@gmail.com>
2019-01-12 01:48:52 -0600

    server

---
+++ client/build/precache-manifest.f4a7fa4fa8fc5b2bd8fe5c8b159ab32c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "259f6e1e018b12ea8760",
+    "url": "/static/js/main.259f6e1e.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "259f6e1e018b12ea8760",
+    "url": "/static/css/main.9f28bbc4.chunk.css"
+  },
+  {
+    "revision": "dff32be92ba0d04bdaa0ff5fd874dbbf",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -2,16 +2,24 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require('mongoose');
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Define API routes here
+
+const books = require('./routes/api/books')
+const db = require('./config/keys').mongoURI;
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks")
+
+app.use('/api/books', books);
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
-// Define API routes here
 
 // Send every other request to the React app
 // Define any API routes before this runs
